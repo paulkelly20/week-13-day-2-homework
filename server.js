fe745d2a3abba7f2680bad3b2b5f4fe69c019c21
Paul Kelly <paul.kelly20@gmail.com>
2018-06-19 20:08:58 +0100

    Added for to make new player

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const parser = require('body-parser');
 const server = express();
 const MongoClient = require('mongodb').MongoClient;
 const MongoId = require('mongodb').ObjectID;
+const { body,validationResult } = require('express-validator/check');
+const  sanitizeBody  = require('express-validator/filter');
 
 
 server.use(parser.json());
@@ -14,6 +16,11 @@ MongoClient.connect("mongodb://localhost:27017", function(err, client, next){
 
   const db = client.db('playersDb')
   console.log("Connected");
+
+
+  server.get('/players/new', function(req, res){
+  res.sendFile(__dirname + '/newPlayer.html')
+});
 
   server.get("/players/:id", function(req, res){
     const players = db.collection('playersCollection');
@@ -42,7 +49,7 @@ MongoClient.connect("mongodb://localhost:27017", function(err, client, next){
     players.remove({_id: mongoId}, function(err, next){
       if (err) next(err);
       req.method = "GET";
-      res.redirect(201, "players");
+      res.redirect(201, "/players");
     })
   })
 
@@ -51,20 +58,36 @@ MongoClient.connect("mongodb://localhost:27017", function(err, client, next){
     players.remove({}, function(err, next){
       if(err) next(err);
       req.method = 'GET';
-      res.redirect(201, "players");
+      res.redirect(201, "/players");
     });
   });
 
+  // server.post("/players", function(req, res){
+  //   const players = db.collection('playersCollection');
+  //   const newPlayer = req.body;
+  //   players.save(newPlayer, function(err, savedPlayer, next){
+  //     if(err) next(err);
+  //     res.status(201);
+  //     res.json(savedPlayer.ops[0])
+  //     console.log("Player Saved");
+  //   })
+  // })
+
   server.post("/players", function(req, res){
     const players = db.collection('playersCollection');
-    const newPlayer = req.body;
+    const name = req.body.name;
+    const nation = req.body.nation;
+    const squadNumber = req.body.number
+    newPlayer = {name: name, nation: nation, number: squadNumber }
     players.save(newPlayer, function(err, savedPlayer, next){
       if(err) next(err);
-      res.status(201);
-      res.json(savedPlayer.ops[0])
+      req.method = 'GET';
+      res.redirect(301, "/players");
       console.log("Player Saved");
     })
   })
+
+
 
   server.get("/players",function(req, res){
     const players = db.collection('playersCollection');
